eab9b759ad0ce4563d8bbcd67acbe8dce196dec6
Lea Hwang <dbwjd5032@gmail.com>
2024-09-25 17:49:49 +0900

    programmers 타겟 넘버
    - DFS를 활용해 풀이했습니다.

--- src/main/java/com/algorithm/problemsolving/java/programmers/TargetNumber.java
+++ src/main/java/com/algorithm/problemsolving/java/programmers/TargetNumber.java
@@ -1,33 +1,29 @@
 package com.algorithm.problemsolving.java.programmers;
 
 /**
- * [고득점 kit] 코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버
- * 완전탐색, dfs 활용
+ * [고득점 kit, 복습] 코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버 - dfs
  *
  * 문제: https://school.programmers.co.kr/learn/courses/30/lessons/43165
  */
 public class TargetNumber {
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, target, 0, 0);
+        return dfs(0, numbers, target, 0);
     }
 
-    public static int dfs(int[] numbers, int target, int currentSum, int step){
-        // 반환할 값
+    // dfs
+    private static int dfs(int depth, int[] numbers, int target, int sum) {
         int count = 0;
 
-        // base code : 만약 현재 단계와 numbers 배열의 길이가 같다면 숫자를 모두 확인한 것 +
-        // 현재까지 계산한 값과 target이 같다면 count + 1, 다르면 기존 count 반환
-        if(step == numbers.length){
-            if(currentSum == target){
-                count++;
-                return count;
-            }else{
-                return count;
+        // base code
+        if(depth == numbers.length) {
+            if(target == sum) {
+                count ++;
             }
+            return count;
         }
 
-        // numbers 배열의 원소를 더하거나 빼는 두 가지 경우를 모두 dfs탐색, 다음 단계로 넘어감 step +1
-        return dfs(numbers, target, currentSum + numbers[step], step + 1) +
-        dfs(numbers, target, currentSum - numbers[step], step + 1);
+        count += dfs(depth+1, numbers, target, sum + numbers[depth]);
+        count += dfs(depth+1, numbers, target, sum - numbers[depth]);
+        return count;
     }
 }
