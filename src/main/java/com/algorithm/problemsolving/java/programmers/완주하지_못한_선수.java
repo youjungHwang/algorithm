5c7019076eb956e741af7da751090340d15b9547
Lea Hwang <dbwjd5032@gmail.com>
2024-09-27 12:31:40 +0900

    programmers 완주하지_못한_선수
    - hashMap을 활용해 풀이했습니다.

--- src/main/java/com/algorithm/problemsolving/java/programmers/완주하지_못한_선수.java
+++ src/main/java/com/algorithm/problemsolving/java/programmers/완주하지_못한_선수.java
@@ -1,7 +1,6 @@
 package com.algorithm.problemsolving.java.programmers;
 
 import java.util.*;
-
 /**
  * [고득점 kit] 코딩테스트 연습 > 해시 > 완주하지 못한 선수
  *
@@ -11,32 +10,26 @@ public class 완주하지_못한_선수 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        // [주의] 참가자 이름 : 출현 횟수를 저장
-        Map<String, Integer> participantMap = new HashMap<>();
-
-        // 참가자 배열을 순회하며 각 참가자의 이름을 해시맵에 저장하고, 출현 횟수를 1씩 증가
-        for(int i=0; i<participant.length; i++){
-            participantMap.put(participant[i], participantMap.getOrDefault(participant[i], 0) + 1);
+        // participant -> {이름 : 출현 횟수}
+        Map<String, Integer> hashMap = new HashMap<>();
+        for(String person : participant) {
+            hashMap.put(person, hashMap.getOrDefault(person, 0) + 1);
         }
 
-        // 완주자 배열을 순회하며 해시맵에서 해당 이름이 있다면 1씩 감소 (기존데이터를 대체 가능)
-        for(String person : completion){
-            participantMap.put(person, participantMap.get(person)-1) ;
-
-            // 만약 해당 이름의 출현 횟수가 0이 되면 해시맵에서 제거
-            if(participantMap.get(person) == 0){
-                participantMap.remove(person);
+        for(int i=0; i<completion.length; i++) {
+            if(hashMap.containsKey(completion[i])) {
+                int value = hashMap.get(completion[i]);
+                value -= 1;
+                hashMap.put(completion[i], value);
             }
         }
 
-        // 해시맵에서 출현 횟수가 존재하면 answer에 넣고 반환
-        for(String remainPerson : participantMap.keySet()){
-            answer = remainPerson;
-            // 남아있는 이름은 하나이므로 찾으면 바로 break
-            break;
+        // value가 0이 아닌 사람을 반환
+        for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            if(entry.getValue() != 0) {
+                answer = entry.getKey();
+            }
         }
-
         return answer;
     }
-
 }
