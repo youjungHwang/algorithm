37830690741a82274c7d04e99c81f956f15a77ea
Lea Hwang <78125105+youjungHwang@users.noreply.github.com>
2025-02-19 14:16:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1196 ms, Memory: 80192 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class TreeNode {
     int value;
@@ -7,54 +7,100 @@ class TreeNode {
 
     public TreeNode(int value) {
         this.value = value;
-        children = new ArrayList<>();
+        this.children = new ArrayList<>();
     }
 }
 public class Main {
-    // 노드 배열
-    private static TreeNode[] tree;
+    static FastReader scan = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
 
-    // 출력 : 부모 배열
-    private static int[] parents;
+    static TreeNode[] tree;
+    static int n;
+    static boolean[] visited;
+    static int[] parents;
 
-    public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        n = scan.nextInt();
 
-       // 초기화
-       tree = new TreeNode[n+1];
-       parents = new int[n+1];
+        // 트리 초기화
+        tree = new TreeNode[n+1];
+        for(int i=1; i<=n; i++) {
+            tree[i] = new TreeNode(i);
+        }
 
-       for(int i=1; i<=n; i++) {
-           tree[i] = new TreeNode(i);
-       }
+        // 트리 생성
+        for(int i=0; i<n-1; i++) {
+            String[] info = scan.nextLine().split(" ");
+            int first = Integer.parseInt(info[0]);
+            int second = Integer.parseInt(info[1]);
 
-       for(int i=0; i<n-1; i++) {
-           String[] data = br.readLine().split(" ");
-           int first = Integer.parseInt(data[0]);
-           int second = Integer.parseInt(data[1]);
+            tree[first].children.add(tree[second]);
+            tree[second].children.add(tree[first]);
+        }
 
-           tree[first].children.add(tree[second]);
-           tree[second].children.add(tree[first]);
-       }
+        // 방문 배열 초기화
+        visited = new boolean[n+1];
+        parents = new int[n+1];
+        findParentNode(1);
 
-       boolean[] visited = new boolean[n+1];
-       findParent(1, visited);
-
-       // 출력
-       for(int i=2; i<=n; i++) {
+       for(int i=2; i<n+1; i++) {
            System.out.println(parents[i]);
        }
+
+        // 자원 해제
+        scan.close();
+        out.close();
     }
 
-    // dfs
-    private static void findParent(int start, boolean[] visited) {
-        visited[start] = true;
+    private static void findParentNode(int curr) {
+        visited[curr] = true;
 
-        for(TreeNode child : tree[start].children) {
+        for(TreeNode child : tree[curr].children) {
             if(!visited[child.value]) {
-                parents[child.value] = start;
-                findParent(child.value, visited);
+                parents[child.value] = curr;
+                findParentNode(child.value); 
+            }
+        }
+    }
+
+    static class FastReader {
+        private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer tokenizer;
+
+        String nextString() {
+            while (tokenizer == null || !tokenizer.hasMoreElements()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        long nextLong() {
+            return Long.parseLong(nextString());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        void close() {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
