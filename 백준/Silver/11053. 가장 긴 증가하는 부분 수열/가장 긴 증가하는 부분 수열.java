76898ee62cfa7c489b46c79f2fc1a443c361833a
Lea Hwang <78125105+youjungHwang@users.noreply.github.com>
2025-03-07 18:13:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,44 +5,42 @@ public class Main {
     static FastReader scan = new FastReader();
     static PrintWriter out = new PrintWriter(System.out);
 
-    static int n;
     static int[] arr;
     static int[] dp;
+    static int n;
 
     public static void main(String[] args) {
-        // dp[i] = i번째 까지의 가장 긴 부분 수열의 길이
-        // 투 포인터 사용 -> 배열 갱신
-        // j < i -> dp[i] = dp[j] + 1 (단, 기존 값보다 작으면 무시)
-
+        // dp[i] = i 번째까지 가장 긴 증가하는 부분 수열
         n = scan.nextInt();
-
         arr = new int[n];
-        String[] info = scan.nextLine().split(" ");
-        for(int i=0; i<n; i++){
-            int num = Integer.parseInt(info[i]);
-            arr[i] = num;
-        }
 
         dp = new int[n];
+
+        String[] info = scan.nextLine().split(" ");
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(info[i]);
+        }
+
+        // dp 초기값 1로 세팅
         Arrays.fill(dp, 1);
 
-        // 투 포인터로 dp배열 갱신
-       int max = moveDpArr();
-
-        out.print(max);
+        // 투포인터로 dp 배열 갱신
+        int result = findMaxLength();
+        out.print(result);
 
         // 자원 해제
         scan.close();
         out.close();
     }
 
-    private static int moveDpArr(){
+    private static int findMaxLength() {
         int max = 1;
 
-        // 투 포인터
-        for(int i=0; i<n; i++) { // 이동하는 포인터
-            for(int j=0; j<i; j++) { // 비교하는 포인터
-                if(arr[j] < arr[i]) {
+        // i : 이동하는 포인터
+        // j : i와 비교하는 포인터
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(arr[i] > arr[j]) {
                     if(dp[i] < dp[j] + 1) {
                         dp[i] = dp[j] + 1;
                         max = Math.max(max, dp[i]);
@@ -50,6 +48,7 @@ public class Main {
                 }
             }
         }
+
         return max;
     }
 
@@ -95,3 +94,5 @@ public class Main {
         }
     }
 }
+
+
